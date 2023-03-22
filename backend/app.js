c6a9c4cffb689acfa47c59730833af5ffdb2444f
Sanjay Kumar <sanjaysewa999@gmail.com>
2023-03-23 00:02:30 +0530

    start front-end

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,10 @@ app.use(cookieParser());
 // Route Import
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoutes");
+const order = require("./routes/orderRoute");
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 // Middleware for error
 app.use(errorMiddleware);
